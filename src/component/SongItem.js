f1ee7e6f3fc2ec37d4b9996fa306d83d52db671b
Neil Watkins <neil.watkins1@sky.com>
2019-04-11 15:17:32 +0100

    css fixed

--- src/component/SongItem.js
+++ src/component/SongItem.js
@@ -5,12 +5,11 @@ const SongItem = (props) => {
 
   return (
     <div className="song-item">
-    <h2>Chart Position: {props.position}</h2>
-  <h2>Title: {props.song["im:name"]["label"]}</h2>
+    <h2 className="position" >Chart Position: {props.position}</h2>
+  <h2  className="title" >Title: {props.song["im:name"]["label"]}</h2>
   <h3>Artist: {props.song["im:artist"]["label"]}</h3>
   <img src={props.song["im:image"][2]["label"]} alt="Artwork"/>
-  <br/>
-  <br/>
+  
   <audio className="controls" controls="play" src={props.song.link[1].attributes.href}></audio>
     </div>
 
